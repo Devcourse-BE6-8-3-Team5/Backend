d7978c4350a3fa7b036fd52cfc3718fcafa8b6a9
yeeJin227 <dpwls99600@gmail.com>
2025-08-28 15:13:11 +0900

    fix: TestRq 생성자에서 null대신 mock객체 사용 (#46)

--- src/test/java/com/back/backend/global/rq/TestRq.java
+++ src/test/java/com/back/backend/global/rq/TestRq.java
@@ -1,14 +1,18 @@
 package com.back.backend.global.rq;
 
 import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.service.MemberService;
 import com.back.global.rq.Rq;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import static org.mockito.Mockito.mock;
 
 public class TestRq extends Rq {
     private Member actor;
 
     public TestRq() {
-        // 실제 req, resp, service는 사용되지 않으므로 null 전달
-        super(null, null, null);
+        // 기존 null에서 Mock 객체로 변경
+        super(mock(HttpServletRequest.class), mock(HttpServletResponse.class), mock(MemberService.class));
     }
 
     public void setActor(Member actor) {
