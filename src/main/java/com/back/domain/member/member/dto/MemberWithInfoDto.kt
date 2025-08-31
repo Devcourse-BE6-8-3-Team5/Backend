c66d588206b946d1e44009d2573fd54fc4ce8aca
SeongChoel <tldtlddl1234@gmail.com>
2025-08-31 12:00:38 +0900

    MemberWithRankDto 변환

--- src/main/java/com/back/domain/member/member/dto/MemberWithInfoDto.kt
+++ src/main/java/com/back/domain/member/member/dto/MemberWithInfoDto.kt
@@ -3,26 +3,25 @@ package com.back.domain.member.member.dto
 import com.back.domain.member.member.entity.Member
 import com.back.global.util.LevelSystem.calculateLevel
 import com.back.global.util.LevelSystem.getImageByLevel
-import lombok.Getter
 
 //경험치,레벨까지 포함한 DTO
-@Getter
-class MemberWithInfoDto(member: Member) {
-    private val id: Long?
-    private val name: String?
-    private val email: String?
-    private val exp: Int
-    private val level: Int
-    private val role: String?
-    private val characterImage: String? // 레벨에 따른 캐릭터 이미지
+class MemberWithInfoDto(
+    val id: Long,
+    val name: String,
+    val email: String,
+    val exp: Int,
+    val level: Int,
+    val role: String,
+    val characterImage: String
+) {
 
-    init {
-        this.id = member.id
-        this.name = member.name
-        this.email = member.email
-        this.exp = member.exp
-        this.level = calculateLevel(member.exp)
-        this.characterImage = getImageByLevel(level)
-        this.role = member.role
-    }
+    constructor(member: Member): this(
+        id = member.id,
+        name = member.name,
+        email = member.email,
+        exp = member.exp,
+        level = calculateLevel(member.exp),
+        characterImage = getImageByLevel(calculateLevel(member.exp)),
+        role = member.role
+    )
 }
