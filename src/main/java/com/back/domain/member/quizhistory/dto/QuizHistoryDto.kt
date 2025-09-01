b92c10404d349bdfe2289383c7aabba5ab159cd2
SeongChoel <tldtlddl1234@gmail.com>
2025-09-01 18:52:46 +0900

    코드 수정

--- src/main/java/com/back/domain/member/quizhistory/dto/QuizHistoryDto.kt
+++ src/main/java/com/back/domain/member/quizhistory/dto/QuizHistoryDto.kt
@@ -22,7 +22,7 @@ class QuizHistoryDto(
         answer = quizHistory.answer,
         isCorrect = quizHistory.isCorrect,
         gainExp = quizHistory.gainExp,
-        createdDate = quizHistory.createdDate.toString(),
+        createdDate = quizHistory.createdDate?.toString() ?: throw ServiceException(400, "퀴즈 풀이 시간이 존재하지 않습니다."),
         memberId = quizHistory.member?.id ?: throw ServiceException(400, "퀴즈 히스토리의 유저 정보가 존재하지 않습니다."),
         memberName = quizHistory.member?.name
     )
